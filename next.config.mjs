63a7cb9b3d09c2b030c52caca282aca0369412c4
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-07-20 23:28:36 +0100

    ImagesPathUpdate

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname:
+          'www.depilacion-laser-git-main-enriquesuarezzzs-projects.vercel.app/',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
